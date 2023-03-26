77472a5ae92cc27129cc862c50d935e1ec6607f4
dmakger <dmakger@yandex.ru>
2023-03-27 01:55:17 +0300

    Добавил модели для Персоны (Актер, Режиссер, Оператор...)

--- api/core/models.py
+++ api/core/models.py
@@ -10,3 +10,35 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+
+# ======{ Персона (Актер, Режиссер...) }======
+
+class Person(models.Model):
+    lastname = models.CharField(max_length=32, blank=True, null=True, default=None)
+    username = models.CharField(max_length=32)
+    pastname = models.CharField(max_length=32, blank=True, null=True, default=None)
+    image = models.ImageField(null=True, blank=True, default=None)
+
+    def __str__(self):
+        name = self.username
+        if self.lastname is not None:
+            name = f"{self.lastname} {name}"
+        if self.pastname is not None:
+            name = f"{name} {self.pastname}"
+        return name
+
+
+class Career(models.Model):
+    title = models.CharField(max_length=32, unique=True)
+
+    def __str__(self):
+        return self.title
+
+
+class PersonToCareer(models.Model):
+    person = models.ForeignKey(to=Person, on_delete=models.CASCADE)
+    career = models.ForeignKey(to=Career, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.person} - {self.career}"
+
