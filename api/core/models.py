fb39975076ea5a564a20ee3daffb3bce546bd49d
dmakger <dmakger@yandex.ru>
2023-03-27 01:54:35 +0300

    Добавил модель Профиля

--- api/core/models.py
+++ api/core/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+# ======{ Профиль }======
+class Profile(models.Model):
+    user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True, default=None)
+
+    def __str__(self):
+        return self.user.username
+
